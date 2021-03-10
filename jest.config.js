224ed40608f1ad3855201ce280b681e5f3e4ad80
Matthew Keil <me@matthewkeil.com>
2021-03-10 19:40:27 -0300

    updated project config files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 process.env.NODE_ENV = "testing";
 module.exports = {
   roots: ["./"],
-  // preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.+(ts|tsx)", "**/?(*.)+(spec|test).+(ts|tsx)"],
   transform: { "^.+\\.(ts|tsx)$": "ts-jest" }
